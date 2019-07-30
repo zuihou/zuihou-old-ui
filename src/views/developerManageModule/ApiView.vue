093ce2cfccd4d9b156f310aa3f41b2717962d77d
tangyh <244387066@qq.com>
2019-07-30 09:26:20 +0800

    修复bug

--- src/views/developerManageModule/ApiView.vue
+++ src/views/developerManageModule/ApiView.vue
@@ -3,15 +3,19 @@
     <apiSearchCondition ref="apiSearchCondition" @onSearch="preSearch" @onCreate="openDialog('editDialog', null, 'create')"></apiSearchCondition>
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column prop="code" label="编码" width="200"></el-table-column>
-      <el-table-column prop="name" label="名称" width="80"></el-table-column>
+      <el-table-column prop="name" label="名称" minWidth="80"></el-table-column>
       <el-table-column prop="menuName" label="关联菜单" width="80"></el-table-column>
       <el-table-column prop="tags" label="类标签" width="200"></el-table-column>
       <el-table-column prop="describe" label="接口描述" width="200"></el-table-column>
       <el-table-column prop="uri" label="地址" width="200"></el-table-column>
-      <el-table-column prop="httpMethod.desc" label="请求方式" width="50"></el-table-column>
-      <el-table-column prop="deprecated" label="是否过时" width="50"></el-table-column>
-      <el-table-column prop="createTime" label="创建时间" width="80"></el-table-column>
-      <el-table-column prop="updateTime" label="更新时间" width="80"></el-table-column>
+      <el-table-column prop="httpMethod.desc" label="请求方式" width="100"></el-table-column>
+      <el-table-column prop="deprecated" label="是否过时" width="50">
+        <template slot-scope="scope">
+          {{scope.row.deprecated ? '是' : '否'}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="createTime" label="创建时间" width="190"></el-table-column>
+      <el-table-column prop="updateTime" label="更新时间" width="190"></el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -34,6 +38,7 @@ export default {
         pageNo: 1,
         pageSize: 10
       }
+      //, tableData: [{'code': '123', 'deprecated': true}]
     }
   },
   methods: {
--- src/views/developerManageModule/serviceManage.vue
+++ src/views/developerManageModule/serviceManage.vue
@@ -2,12 +2,12 @@
   <el-card>
     <searchCondition ref="searchCondition" @onSearch="preSearch" @onCreate="openDialog('editDialog', null, 'create')"></searchCondition>
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column prop="eurekaCode" label="服务id" width="200"></el-table-column>
-      <el-table-column prop="name" label="服务名称" width="80"></el-table-column>
+      <el-table-column prop="eurekaCode" label="服务id" width="250"></el-table-column>
+      <el-table-column prop="name" label="服务名称" minWidth="80"></el-table-column>
       <el-table-column prop="describe" label="服务描述" width="80"></el-table-column>
-      <el-table-column prop="swaggerUrl" label="接口地址" width="200"></el-table-column>
-      <el-table-column prop="updateTime" label="更新时间" width="80"></el-table-column>
-      <el-table-column fixed="right" label="操作" width="150">
+      <el-table-column prop="swaggerUrl" label="接口地址" width="300"></el-table-column>
+      <el-table-column prop="updateTime" label="更新时间" width="190"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="200">
         <template slot-scope="scope">
           <el-button type="text" size="small" @click="onParse(scope.row)">解析接口</el-button>
           <el-button type="text" size="small" @click="openDialog('apiViewDialog', scope.row, 'view')">查看接口</el-button>
